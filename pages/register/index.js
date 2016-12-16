eff9002ae40c3bf7faa18fc319d2918d8613e473
rolspace <rolandorramos@gmail.com>
2016-12-16 23:07:06 +0100

    Fixed Register component

--- pages/register/index.js
+++ pages/register/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Register extends React.Component {
 	render() {
-		return <div>This is the Registration Page!</div>
+		return (
+			<div>
+				This is the Registration Page!
+			</div>
+		)
 	}
 }
 
