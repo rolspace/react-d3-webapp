c7bc78f8af488d1257839f0a8b9208f9a3b69651
rolspace <rolandorramos@gmail.com>
2017-02-22 23:54:28 +0100

    Read query string code from Instagram and added eslint ignore rule for testing

--- pages/start/index.js
+++ pages/start/index.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-console */
+
 import React from 'react';
 import AuthorizedOptionsList from '../../containers/AuthorizedOptionsList';
 
 class StartPage extends React.Component {
+	componentDidMount() {
+		console.log(this.props.location.query.code);
+	}
+	
 	render() {
 		return (
 			<div>
