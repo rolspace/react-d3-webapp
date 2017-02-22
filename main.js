795ac95c6074f307097e34e61e1abfe00d39d721
rolspace <rolandorramos@gmail.com>
2017-02-22 23:28:14 +0100

    Made modifications to support browserhistory instead of hashhistory

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import store from './core/store';
 import App from './components/App';
 import HomePage from './pages/home';
@@ -12,7 +12,7 @@ const container = document.getElementById('container');
 
 ReactDom.render(
 	<Provider store={store}>
-		<Router history={hashHistory}>
+		<Router history={browserHistory}>
 			<Route path="/" component={App}>
 				<IndexRoute component={HomePage}/>
 				<Route path="/start" component={StartPage}/>
