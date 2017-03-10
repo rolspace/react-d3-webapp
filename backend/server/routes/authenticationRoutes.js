be9a1a7ece65996fd5147e948e3b0b952945649c
rolspace <rolandorramos@gmail.com>
2017-03-10 21:48:57 +0100

    Added code to test real method call to express server

--- backend/server/routes/authenticationRoutes.js
+++ backend/server/routes/authenticationRoutes.js
@@ -2,7 +2,7 @@ const config = require('config');
 const express = require('express');
 
 function postAuthentication(req, res) {
-	res.status(config.httpOk).end();
+	res.status(config.httpOk).send('postAuthentication method called').end();
 }
 
 const router = express.Router();
