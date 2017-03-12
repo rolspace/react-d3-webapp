745dd4b5fd5dd5b1cee61afce5d5b8fd726ca22d
rolspace <rolandorramos@gmail.com>
2017-03-12 23:15:44 +0100

    Action creators for authorization async request

---
+++ client/actions/receive-authorization.js
@@ -0,0 +1,4 @@
+import { createAction } from 'redux-actions';
+
+export const RECEIVE_AUTHORIZATION = 'RECEIVE_AUTHORIZATION';
+export const ReceiveAuthorization = createAction(RECEIVE_AUTHORIZATION);
---
+++ client/actions/request-authorization.js
@@ -0,0 +1,4 @@
+import { createAction } from 'redux-actions';
+
+export const REQUEST_AUTHORIZATION = 'REQUEST_AUTHORIZATION';
+export const GetAuthorization = createAction(REQUEST_AUTHORIZATION);
