3dd75b3c948b13d9a3f67030be6f006b139bbb55
Rolando Ramos <rolandorramos@gmail.com>
2017-03-26 23:46:12 +0200

    Added working version of the backend service request to get the Instagram token

--- backend/server/routes/authenticationRoutes.js
+++ backend/server/routes/authenticationRoutes.js
@@ -3,8 +3,19 @@ const cors = require('cors');
 const express = require('express');
 const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 function postAuthentication(req, res) {
+	var form = {
+		client_id: config.client.id,
+		client_secret: config.client.secret,
+		redirect_uri: config.client.redirect_uri,
+		grant_type: 'authorization_code',
+		code: req.body.code
+	}
+	
+	request.post({ url: 'https://api.instagram.com/oauth/access_token', form: form }, function(error, response, body) {
+		console.log(body);
 	});
 
 	res.status(config.http.Ok).send('postAuthentication method called').end();
