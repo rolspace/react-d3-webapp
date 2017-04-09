62f40b80f185db3ed3a5f3e31fa29078325ae3dd
Rolando Ramos <rolandorramos@gmail.com>
2017-04-09 23:55:15 +0200

    Added correct error status variable to console.log

--- backend/server/routes/authentication.js
+++ backend/server/routes/authentication.js
@@ -36,7 +36,7 @@ function postAuthentication(req, res) {
 
 					user.save((err) => {
 						if (err) {
-							console.log("error");
+							console.log(err);
 						}
 						else {
 							console.log("success");
