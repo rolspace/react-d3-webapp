5c10657aa2232a0c786798dcf650963ff2fe1701
Rolando Ramos <rolandorramos@gmail.com>
2017-04-14 00:54:12 +0200

    Changed property name

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const db = require('../server/db');
 
 const userSchema = mongoose.Schema({
-	'instagram_id': { type: String, index: true, required: true },
+	'id': { type: String, index: true, required: true },
 	'profile_picture': { type: String },
 	'username': { type: String	, required: true },
 	'token': { type: String, required: true },
@@ -17,10 +17,10 @@ userSchema.pre('save', function(callback) {
 	bcrypt.genSalt(10, (err, salt) => {
 		if (err) return callback(err);
 
-		bcrypt.hash(user.instagram_id, salt, (err, hash) => {
+		bcrypt.hash(user.id, salt, (err, hash) => {
 			if (err) return callback(err);
 
-			user.instagram_id = hash;
+			user.id = hash;
 			callback();
 		});
 	});
