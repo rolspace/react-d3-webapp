917f24252ee7c986c44da5108806ab4455d3e1e2
rolspace <rolandorramos@gmail.com>
2017-04-17 23:20:27 +0200

    Added jsonapi Deserializer for user data

--- client/core/jsonapi.js
+++ client/core/jsonapi.js
@@ -1,7 +1,10 @@
 const JSONAPISerializer = require('jsonapi-serializer').Serializer;
+const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
 
 const authorizationSerializer = new JSONAPISerializer('authorizations', { attributes: [ 'code' ] });
+const userDeserializer = new JSONAPIDeserializer('users');
 
 module.exports = {
-	authorizationSerializer: authorizationSerializer
+	authorizationSerializer: authorizationSerializer,
+	userDeserializer: userDeserializer
 };
