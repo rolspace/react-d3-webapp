55a1289c59bad5171b96ae46deccae9afcf24b21
Rolando Ramos <rolandorramos@gmail.com>
2017-04-19 23:21:22 +0200

    Removed request-authorization action

--- client/actions/receive-authorization.js
+++
@@ -1,9 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const RECEIVE_AUTHORIZATION = 'RECEIVE_AUTHORIZATION';
-export const receiveAuthorization = createAction(RECEIVE_AUTHORIZATION, (authorized, json) => {
-	return {
-		authorized: authorized,
-		user: json.data
-	}
-});
