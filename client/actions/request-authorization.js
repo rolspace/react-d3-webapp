7c633f953c2edbae1dd38093e9261a47c3f33b6e
Rolando Ramos <rolandorramos@gmail.com>
2017-04-19 23:22:32 +0200

    Finished functionality to create cookie with user data

--- client/actions/request-authorization.js
+++ client/actions/request-authorization.js
@@ -7,8 +7,8 @@ import 'whatwg-fetch';
 export const REQUEST_AUTHORIZATION = 'REQUEST_AUTHORIZATION';
 export const requestAuthorization = createAction(REQUEST_AUTHORIZATION, /* async */code => {
 	var payload = jsonapi.authorizationSerializer.serialize({ code: code });
-	//const result = 
-	/* await */fetch('http://localhost:4000/api/authorization/', {
+	
+	const result = /* await */fetch('http://localhost:4000/api/authorization/', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
@@ -28,9 +28,9 @@ export const requestAuthorization = createAction(REQUEST_AUTHORIZATION, /* async
 				throw new Error(error);
 			}
 			else {
-				const today = new Date();
-				const tomorrow = today.getDate()
-				document.cookie = `id=${users.id};expires=${(new Date().toUTCString())};path=/`;
+				const date = new Date();
+				date.setDate(date.getDate() + 14);
+				document.cookie = `id=${users.id};expires=${(date.toUTCString())};path=/`;
 			}
 		})
 	}).catch(error => {
