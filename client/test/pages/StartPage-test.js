56efeac718db4f04fdd73f57f769a4e2346e2b9b
Rolando Ramos <rolandorramos@gmail.com>
2017-05-01 23:34:24 +0200

    Added missing prop for test condition

--- client/test/pages/StartPage-test.js
+++ client/test/pages/StartPage-test.js
@@ -5,7 +5,8 @@ import StartPage from '../../pages/start';
 
 describe('StartPage Component', function() {
 	it('should render one <div> element', function() {
-		const wrapper = shallow(<StartPage />);
+
+		const wrapper = shallow(<StartPage location={{ query: { code: 'some-code' }}} />);
 		expect(wrapper.find('div')).to.have.length(1);
 	});
 });
