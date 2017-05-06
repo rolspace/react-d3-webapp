76987126fa3986e105825fa9d48f27d375d42fc6
rolspace <rolandorramos@gmail.com>
2017-05-07 01:03:13 +0200

    Removed commented code

--- backend/server/routes/authorize.js
+++ backend/server/routes/authorize.js
@@ -10,49 +10,6 @@ function postAuthorize(req, res) {
 	}
 	else {
 		utils.logger.info(req.body);
-		// jsonApi.authorizationDeserializer.deserialize(req.body, (error, authorization) => {
-		// 	if (error) {
-		// 		res.status(config.http.internalError).send(new jsonApi.Error({ detail: 'Internal server error' }));			
-		// 	}
-
-		// 	var form = {
-		// 		client_id: config.client.id,
-		// 		client_secret: config.client.secret,
-		// 		code: authorization.code,
-		// 		grant_type: 'authorization_code',
-		// 		redirect_uri: config.client.redirect_uri
-		// 	}
-
-		// 	request.post({ url: 'https://api.instagram.com/oauth/access_token', form: form },
-		// 		function(error, response, body) {
-		// 			utils.logger.info(response);
-
-		// 			if (error) {
-		// 				res.status(config.http.internalError).send(new jsonApi.Error({ detail: 'Internal server error' }));
-		// 			}
-		// 			else if (response.statusCode !== config.http.ok) {
-		// 				res.status(response.statusCode).send(new jsonApi.Error({ detail: 'Connection to external provider failed' }));
-		// 			}
-		// 			else {
-		// 				body = JSON.parse(response.body);
-
-		// 				const user = new UserModel({
-		// 					id: body.user.id,
-		// 					token: body.access_token,
-		// 					username: body.user.username
-		// 				});
-
-		// 				user.save((error) => {
-		// 					if (error) {
-		// 						res.status(config.http.internalError).send(new jsonApi.Error({ detail: 'Internal server error' }));
-		// 					}
-		// 					else {
-		// 						res.status(config.http.ok).send(jsonApi.userSerializer.serialize({ id: user.id }));
-		// 					}
-		// 				});
-		// 			}
-		// 		});
-		// });
 
 		jsonApi.authorizationDeserializer.deserialize(req.body)
 		.then((authorization) => {
