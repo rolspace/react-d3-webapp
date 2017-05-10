0e83264caa2001cdf6a714ed3dd6f14031e2875b
Rolando Ramos <rolandorramos@gmail.com>
2017-05-10 23:35:42 +0200

    Added additional property to serialize on JsonAPI responses

--- backend/common/jsonapi.js
+++ backend/common/jsonapi.js
@@ -3,7 +3,7 @@ const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
 const JSONAPIError = require('jsonapi-serializer').Error;
 
 const authorizationDeserializer = new JSONAPIDeserializer('authorizations');
-const userSerializer = new JSONAPISerializer('users', { attributes: [ ] });
+const userSerializer = new JSONAPISerializer('users', { attributes: [ 'username' ] });
 const userDeserializer = new JSONAPIDeserializer('users');
 
 module.exports = {
