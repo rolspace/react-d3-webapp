70733d3e207ba7b63ae73ccf13034473dc3a8dbf
Rolando Ramos <rolandorramos@gmail.com>
2017-05-17 02:12:15 +0200

    Modified property name

--- client/components/Button.js
+++ client/components/Button.js
@@ -3,13 +3,13 @@ import React from 'react';
 class Button extends React.Component {
 	render() {
 		return (
-			<button onClick={this.props.onClick}>{this.props.text}</button>
+			<button onClick={this.props.click}>{this.props.text}</button>
 		);
 	}
 }
 
 Button.propTypes = {
-	onClick: React.PropTypes.func,
+	click: React.PropTypes.func,
 	text: React.PropTypes.string.isRequired	
 }
 
