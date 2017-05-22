d1a23f90a7bb4b727bfa04afaf231cf5c05abab1
Rolando Ramos <rolandorramos@gmail.com>
2017-05-22 09:10:17 +0200

    Minor code clean up

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,8 +1,8 @@
 const config = require('config');
 const utils = require('../common/utils');
 
-const http = require('http');
 const db = require('./db');
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -18,15 +18,14 @@ function init() {
 	app.use(bodyParser.json())
 	app.use(cors(config.cors));
 	
-	app.post('/api/authorization/', authorization.post);
 	app.get('/api/user/:id', user.get);
+	app.post('/api/authorization/', authorization.post);
 	
 	app.use((req, res) => {
 		res.status(config.http.notFound).send({ 'message': 'Resource not found' });
 	});
 
 	app.set('port', config.port);
-
 	db.init();
 }
 
