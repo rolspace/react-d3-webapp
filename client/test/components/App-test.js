35529e9277b712b7887744fd3f424c21747e6356
Rolando Ramos <rolandorramos@gmail.com>
2017-05-25 01:17:12 +0200

    Added new unit test for the App component

--- client/test/components/App-test.js
+++ client/test/components/App-test.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { expect } from 'chai';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from '../../components/App';
 
 describe('App Component', function() {
 	it('should be wrapped by one <div> element', function() {
-		const wrapper = render(<App>App Component</App>);
+		const wrapper = shallow(<App>App Component</App>);
 		expect(wrapper.find('div')).to.have.length(1);
 	});
 
-	/*it('should have a "children" property', function() {
-		const wrapper = shallow(<App>App Component</App>);
-		expect(wrapper.find('div')).to.have.length(1);
-	});*/
+	it('should have a "children" property', function() {
+		const wrapper = shallow(<App><span>child1</span><span>child2</span></App>);
+		expect(wrapper.find('span')).to.have.length(2);
+	});
 });
