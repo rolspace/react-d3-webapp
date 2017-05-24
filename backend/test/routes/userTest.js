c531137fc61d41ff62efa8fdc071db3e1892847a
Rolando Ramos <rolandorramos@gmail.com>
2017-05-24 19:40:08 +0200

    Added higher lever describe statement

--- backend/test/routes/userTest.js
+++ backend/test/routes/userTest.js
@@ -8,69 +8,71 @@ const sinonStubPromise = require('sinon-stub-promise');
 
 sinonStubPromise(sinon);
 
-describe('/GET user', () => {
-	beforeEach(() => {
-		//stub logger to prevent console messages
-		loggerStub = sinon.stub(utils.logger, 'info', () => { });
+describe('User Route', () => {
+	describe('/GET user', () => {
+		beforeEach(() => {
+			//stub logger to prevent console messages
+			loggerStub = sinon.stub(utils.logger, 'info', () => { });
 
-		res = {
-			send: sinon.stub().returnsThis(),
-			status: sinon.stub().returnsThis()
-		};
-	});
+			res = {
+				send: sinon.stub().returnsThis(),
+				status: sinon.stub().returnsThis()
+			};
+		})
 
-	afterEach(() => {
-		//restore logger
-		loggerStub.restore();
-	});
+		afterEach(() => {
+			//restore logger
+			loggerStub.restore();
+		})
 
-	it('returns a 422 http status if the id parameter is empty', () => {
-		let req = {};
+		it('returns a 422 http status if the id parameter is empty', () => {
+			let req = {};
 
-		userRoute.get(req, res);
+			userRoute.get(req, res);
 
-		sinon.assert.calledWith(res.status, 422);
-	});
+			sinon.assert.calledWith(res.status, 422);
+		})
 
-	it('returns a 200 http status if the user id exists', () => {
-		let req = {
-			params: {
-				id: 'some-id'
-			}
-		};
+		it('returns a 200 http status if the user id exists', () => {
+			let req = {
+				params: {
+					id: 'some-id'
+				}
+			};
 
-		let result = {
-			id: 'some-id',
-			username: 'some username'
-		};
+			let result = {
+				id: 'some-id',
+				username: 'some username'
+			};
 
-		const findOnePromiseStub = sinon.stub(UserModel, 'findOne').returnsPromise();
-		findOnePromiseStub.resolves(result);
+			const findOnePromiseStub = sinon.stub(UserModel, 'findOne').returnsPromise();
+			findOnePromiseStub.resolves(result);
 
-		userRoute.get(req, res);
-		findOnePromiseStub.restore();
+			userRoute.get(req, res);
+			findOnePromiseStub.restore();
 
-		sinon.assert.calledWith(res.status, 200);
-	});
+			sinon.assert.calledWith(res.status, 200);
+		})
 
-	it('returns a 500 http status if the user id does not exist', () => {
-		let req = {
-			params: {
-				id: 'some-id'
-			}
-		};
+		it('returns a 500 http status if the user id does not exist', () => {
+			let req = {
+				params: {
+					id: 'some-id'
+				}
+			};
 
-		let result = {
-			id: 'some-id',
-			username: 'some username'
-		};
+			let result = {
+				id: 'some-id',
+				username: 'some username'
+			};
 
-		const findOnePromiseStub = sinon.stub(UserModel, 'findOne').returnsPromise();
-		findOnePromiseStub.rejects('Some error');
+			const findOnePromiseStub = sinon.stub(UserModel, 'findOne').returnsPromise();
+			findOnePromiseStub.rejects('Some error');
 
-		userRoute.get(req, res);
-		findOnePromiseStub.restore();
+			userRoute.get(req, res);
+			findOnePromiseStub.restore();
 
-		sinon.assert.calledWith(res.status, 404);
-	});
-});
+			sinon.assert.calledWith(res.status, 404);
+		})
+	})
+})
