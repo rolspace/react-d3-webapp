4215887a4a3d42ef83bfacb6bbecdd086e069198
Rolando Ramos <rolandorramos@gmail.com>
2017-05-26 14:13:15 +0200

    Updated webpack config to run for v2 and removed commented code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,14 +6,14 @@ const webpack = require('webpack');
 
 const isDebug = global.DEBUG === false ? false : !process.argv.includes('--release');
 const isVerbose = process.argv.includes('--verbose') || process.argv.includes('-v');
-const useHMR = !!global.HMR; // Hot Module Replacement (HMR)
+
+// Hot Module Replacement (HMR)
+const useHMR = !!global.HMR;
 const babelConfig = Object.assign({}, pkg.babel, {
 	babelrc: false,
 	cacheDirectory: useHMR,
 });
 
-// Webpack configuration (main.js => public/dist/main.{hash}.js)
-// http://webpack.github.io/docs/configuration.html
 const config = {
 	// The base directory for resolving the entry option
 	context: __dirname,
@@ -32,9 +32,6 @@ const config = {
 		chunkFilename: isDebug ? '[id].js?[chunkhash]' : '[id].[chunkhash].js',
 		sourcePrefix: '  ',
 	},
-
-	// Switch loaders to debug or release mode
-	debug: isDebug,
 
 	// Developer tool to enhance debugging, source maps
 	// http://webpack.github.io/docs/configuration.html#devtool
@@ -55,28 +52,26 @@ const config = {
 
 	// The list of plugins for Webpack compiler
 	plugins: [
-		new webpack.NoErrorsPlugin(),
+		new webpack.LoaderOptionsPlugin({
+			debug: isDebug
+		}),
+		new webpack.NoEmitOnErrorsPlugin(),
 		new webpack.optimize.OccurrenceOrderPlugin(),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': isDebug ? '"development"' : '"production"',
 			'process.env.CLIENT_ID': '"4cd22e4d9e0a469a889ef29b94669b8c"',
 			__DEV__: isDebug,
 		}),
-		// Emit a JSON file with assets paths
-		// https://github.com/sporto/assets-webpack-plugin#options
-		/*new AssetsPlugin({
-			path: path.resolve(__dirname, './public/dist'),
-			filename: 'assets.json',
-			prettyPrint: true,
-		}),*/
 	],
 
 	// Options affecting the normal modules
 	module: {
-		preLoaders: [
-			{
+		rules: [
+		{
 				test: /\.jsx?$/,
+				enforce: 'pre',
 				loader: 'eslint-loader',
+				options: { failOnError: true },
 				include: [
 					path.resolve(__dirname, './actions'),
 					path.resolve(__dirname, './components'),
@@ -87,9 +82,7 @@ const config = {
 					path.resolve(__dirname, './main.js')
 				],
 				exclude: /node_modules/
-			}
-		],
-		loaders: [
+			},
 			{
 				test: /\.jsx?$/,
 				include: [
@@ -101,11 +94,11 @@ const config = {
 					path.resolve(__dirname, './reducers'),
 					path.resolve(__dirname, './main.js')
 				],
-				loader: `babel-loader?${ JSON.stringify(babelConfig) }`,
+				use: [`babel-loader?${ JSON.stringify(babelConfig) }`]
 			},
 			{
 				test: /\.css/,
-				loaders: [
+				use: [
 					'style-loader',
 					`css-loader?${JSON.stringify({
 						sourceMap: isDebug,
@@ -119,81 +112,58 @@ const config = {
 				],
 			}
 		]
-		/*{
-			test: /\.json$/,
-			exclude: [
-			path.resolve(__dirname, './routes.json'),
-			],
-			loader: 'json-loader',
-		},
-		{
-			test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-			loader: 'url-loader?limit=10000',
-		},
-		{
-			test: /\.(eot|ttf|wav|mp3)$/,
-			loader: 'file-loader',
-		},*/
 	},
 
-	// The list of plugins for PostCSS
-	// https://github.com/postcss/postcss
-	postcss(bundler) {
-		return [
-			// Transfer @import rule by inlining content, e.g. @import 'normalize.css'
-			// https://github.com/postcss/postcss-import
-			require('postcss-import')({ addDependencyTo: bundler }),
-			// W3C variables, e.g. :root { --color: red; } div { background: var(--color); }
-			// https://github.com/postcss/postcss-custom-properties
-			//require('postcss-custom-properties')(),
-			// W3C CSS Custom Media Queries, e.g. @custom-media --small-viewport (max-width: 30em);
-			// https://github.com/postcss/postcss-custom-media
-			//require('postcss-custom-media')(),
-			// CSS4 Media Queries, e.g. @media screen and (width >= 500px) and (width <= 1200px) { }
-			// https://github.com/postcss/postcss-media-minmax
-			//require('postcss-media-minmax')(),
-			// W3C CSS Custom Selectors, e.g. @custom-selector :--heading h1, h2, h3, h4, h5, h6;
-			// https://github.com/postcss/postcss-custom-selectors
-			//require('postcss-custom-selectors')(),
-			// W3C calc() function, e.g. div { height: calc(100px - 2em); }
-			// https://github.com/postcss/postcss-calc
-			//require('postcss-calc')(),
-			// Allows you to nest one style rule inside another
-			// https://github.com/jonathantneal/postcss-nesting
-			//require('postcss-nesting')(),
-			// W3C color() function, e.g. div { background: color(red alpha(90%)); }
-			// https://github.com/postcss/postcss-color-function
-			//require('postcss-color-function')(),
-			// Convert CSS shorthand filters to SVG equivalent, e.g. .blur { filter: blur(4px); }
-			// https://github.com/iamvdo/pleeease-filters
-			//require('pleeease-filters')(),
-			// Generate pixel fallback for "rem" units, e.g. div { margin: 2.5rem 2px 3em 100%; }
-			// https://github.com/robwierzbowski/node-pixrem
-			//require('pixrem')(),
-			// W3C CSS Level4 :matches() pseudo class, e.g. p:matches(:first-child, .special) { }
-			// https://github.com/postcss/postcss-selector-matches
-			//require('postcss-selector-matches')(),
-			// Transforms :not() W3C CSS Level 4 pseudo class to :not() CSS Level 3 selectors
-			// https://github.com/postcss/postcss-selector-not
-			//require('postcss-selector-not')(),
-			// Postcss flexbox bug fixer
-			// https://github.com/luisrudge/postcss-flexbugs-fixes
-			//require('postcss-flexbugs-fixes')(),
-			// Add vendor prefixes to CSS rules using values from caniuse.com
-			// https://github.com/postcss/autoprefixer
-			//require('autoprefixer')(),
-		];
-	},
-
-	// ESLint specific config
-	eslint: {
-		failOnError: true
-	}
+	// postcss(bundler) {
+	// 	return [
+	// 		// Transfer @import rule by inlining content, e.g. @import 'normalize.css'
+	// 		// https://github.com/postcss/postcss-import
+	// 		require('postcss-import')({ addDependencyTo: bundler }),
+	// 		// W3C variables, e.g. :root { --color: red; } div { background: var(--color); }
+	// 		// https://github.com/postcss/postcss-custom-properties
+	// 		//require('postcss-custom-properties')(),
+	// 		// W3C CSS Custom Media Queries, e.g. @custom-media --small-viewport (max-width: 30em);
+	// 		// https://github.com/postcss/postcss-custom-media
+	// 		//require('postcss-custom-media')(),
+	// 		// CSS4 Media Queries, e.g. @media screen and (width >= 500px) and (width <= 1200px) { }
+	// 		// https://github.com/postcss/postcss-media-minmax
+	// 		//require('postcss-media-minmax')(),
+	// 		// W3C CSS Custom Selectors, e.g. @custom-selector :--heading h1, h2, h3, h4, h5, h6;
+	// 		// https://github.com/postcss/postcss-custom-selectors
+	// 		//require('postcss-custom-selectors')(),
+	// 		// W3C calc() function, e.g. div { height: calc(100px - 2em); }
+	// 		// https://github.com/postcss/postcss-calc
+	// 		//require('postcss-calc')(),
+	// 		// Allows you to nest one style rule inside another
+	// 		// https://github.com/jonathantneal/postcss-nesting
+	// 		//require('postcss-nesting')(),
+	// 		// W3C color() function, e.g. div { background: color(red alpha(90%)); }
+	// 		// https://github.com/postcss/postcss-color-function
+	// 		//require('postcss-color-function')(),
+	// 		// Convert CSS shorthand filters to SVG equivalent, e.g. .blur { filter: blur(4px); }
+	// 		// https://github.com/iamvdo/pleeease-filters
+	// 		//require('pleeease-filters')(),
+	// 		// Generate pixel fallback for "rem" units, e.g. div { margin: 2.5rem 2px 3em 100%; }
+	// 		// https://github.com/robwierzbowski/node-pixrem
+	// 		//require('pixrem')(),
+	// 		// W3C CSS Level4 :matches() pseudo class, e.g. p:matches(:first-child, .special) { }
+	// 		// https://github.com/postcss/postcss-selector-matches
+	// 		//require('postcss-selector-matches')(),
+	// 		// Transforms :not() W3C CSS Level 4 pseudo class to :not() CSS Level 3 selectors
+	// 		// https://github.com/postcss/postcss-selector-not
+	// 		//require('postcss-selector-not')(),
+	// 		// Postcss flexbox bug fixer
+	// 		// https://github.com/luisrudge/postcss-flexbugs-fixes
+	// 		//require('postcss-flexbugs-fixes')(),
+	// 		// Add vendor prefixes to CSS rules using values from caniuse.com
+	// 		// https://github.com/postcss/autoprefixer
+	// 		//require('autoprefixer')(),
+	// 	];
+	// }//,
 };
 
 // Optimize the bundle in release (production) mode
 if (!isDebug) {
-	config.plugins.push(new webpack.optimize.DedupePlugin());
 	config.plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: isVerbose } }));
 	config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
 }
