cf7922115d7c6bba0e51389f7e7f8bddcd972cc4
Rolando Ramos <rolandorramos@gmail.com>
2017-06-19 00:40:51 +0200

    Removed unnecessary code

--- backend/common/jsonapi.js
+++ backend/common/jsonapi.js
@@ -4,14 +4,9 @@ const JSONAPIError = require('jsonapi-serializer').Error;
 
 const authorizationDeserializer = new JSONAPIDeserializer('authorizations');
 const userSerializer = new JSONAPISerializer('users', { attributes: [ 'username' ] });
-const userDeserializer = new JSONAPIDeserializer('users');
 
 module.exports = {
 	authorizationDeserializer: authorizationDeserializer,
 	userSerializer: userSerializer,
-	userDeserializer: userDeserializer,
 	Error: JSONAPIError
 };
-
-//Create separate properties for each serializer/deserializer and pass a new reference on the export.
-//Create a single function to serialize/deserialize that receives an object parameter.
