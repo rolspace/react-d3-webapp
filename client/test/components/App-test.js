83ab676f26dd6c5f06ef235117044706eb0d8bba
Rolando Ramos <rolandorramos@gmail.com>
2017-07-07 20:59:21 +0200

    Removed App component children test

--- client/test/components/App-test.js
+++ client/test/components/App-test.js
@@ -8,9 +8,4 @@ describe('App Component', function() {
 		const wrapper = shallow(<App>App Component</App>);
 		expect(wrapper.find('div')).to.have.length(1);
 	});
-
-	it('should have a "children" property', function() {
-		const wrapper = shallow(<App><span>child1</span><span>child2</span></App>);
-		expect(wrapper.find('span')).to.have.length(2);
-	});
 });
