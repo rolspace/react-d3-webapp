985feef5613b4a2c5860662d76834c12fa42f2a3
Rolando Ramos <rolandorramos@gmail.com>
2017-07-07 21:18:57 +0200

    Removed old test and added improved tests to App component

--- client/test/components/App-test.js
+++ client/test/components/App-test.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../../components/App';
+import HomePage from '../../pages/home';
+import PrivateRoute from '../../containers/PrivateRoute';
 
 describe('App Component', function() {
-	it('should be wrapped by one <div> element', function() {
-		const wrapper = shallow(<App>App Component</App>);
-		expect(wrapper.find('div')).to.have.length(1);
+	it('should render one <Provider/> component', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find(Provider)).to.have.length(1);
+	});
+
+	it ('should render one <Homepage/> component', () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find(Provider)).to.have.length(1);
+	});
+
+	it('should render one <PrivateRoute/> component', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find(Provider)).to.have.length(1);
 	});
 });
