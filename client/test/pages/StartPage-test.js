3c586318ff9af047c20d1d8a2ea4202daee04b8a
Rolando Ramos <rolandorramos@gmail.com>
2017-07-08 01:21:51 +0200

    Removed one unnecessary test and added more detailed tests

--- client/test/pages/StartPage-test.js
+++ client/test/pages/StartPage-test.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import StartPage from '../../pages/start';
+import React from 'react'
+import { expect } from 'chai'
+import { shallow, mount } from 'enzyme'
+import createRouterContext from 'react-router-test-context'
+import StartPage from '../../pages/start'
+import RecentMedia from '../../pages/recent'
+import OptionsList from '../../components/OptionsList'
 
 describe('StartPage Component', function() {
-	it('should be wrapped by one <div> element', function() {
-		const wrapper = shallow(<StartPage location={{ query: { code: 'some-code' }}} />);
-		expect(wrapper.find('div')).to.have.length(1);
-	});
-});
+	// const mockStore = configureStore()
+	// let store
+
+	// beforeEach(() => {
+	// 	store = mockStore({});
+	// })
+
+	StartPage.contextTypes = {
+    router: React.PropTypes.object
+  }
+
+	it('should render one <OptionsList> Component', () => {
+		const wrapper = shallow(<StartPage/>)
+		expect(wrapper.find(OptionsList)).to.have.length(1)
+	})
+
+	it('should render one <RecentMedia> Component', () => {
+		const context = createRouterContext()
+
+		const wrapper = shallow(<StartPage/>, { context })
+		expect(wrapper.find(RecentMedia)).to.have.length(1)
+	})
+})
