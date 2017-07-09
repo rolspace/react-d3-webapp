d1a5c07647ca33a1d9626d8671002717a6c74ba8
Rolando Ramos <rolandorramos@gmail.com>
2017-07-09 19:43:14 +0200

    Removed unnecessary code

--- client/common/jsonapi.js
+++ client/common/jsonapi.js
@@ -1,16 +1,9 @@
-/* eslint-disable no-unused-labels */
-
 const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
 
 const authorizationSerializer = new JSONAPISerializer('authorizations', { attributes: [ 'code' ] });
+
 const jsonApiDeserializer = new JSONAPIDeserializer({ keyForAttribute: 'camelCase' });
-//const userDeserializer = new JSONAPIDeserializer('users');
-
-// const media = {
-// 	deserialize: (json) => jsonApiDeserializer.deserialize(json)
-// }
-
 const deserialize = (json) => jsonApiDeserializer.deserialize(json)
 
 module.exports = {
