45390b48dd46b3308dc6c751f4969bbb3957eb4e
Rolando Ramos <rolandorramos@gmail.com>
2017-07-09 15:42:51 +0200

    Cleaned up StartPage and App components unit tests

--- client/test/components/App-test.js
+++ client/test/components/App-test.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React from 'react'
 import { Provider } from 'react-redux'
-import { expect } from 'chai';
-import { shallow, mount } from 'enzyme';
-import App from '../../components/App';
-import HomePage from '../../pages/home';
-import PrivateRoute from '../../containers/PrivateRoute';
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import App from '../../components/App'
+import PrivateRoute from '../../containers/PrivateRoute'
 
 describe('App Component', function() {
 	it('should render one <Provider/> component', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find(Provider)).to.have.length(1);
-	});
-
-	it ('should render one <Homepage/> component', () => {
-		const wrapper = mount(<App />);
-		expect(wrapper.find(Provider)).to.have.length(1);
-	});
+		const wrapper = shallow(<App/>)
+		expect(wrapper.find(Provider)).to.have.length(1)
+	})
 
 	it('should render one <PrivateRoute/> component', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find(Provider)).to.have.length(1);
-	});
-});
+		const wrapper = shallow(<App/>)
+		expect(wrapper.find(PrivateRoute)).to.have.length(1)
+	})
+})
--- client/test/pages/StartPage-test.js
+++ client/test/pages/StartPage-test.js
@@ -1,32 +1,12 @@
 import React from 'react'
 import { expect } from 'chai'
-import { shallow, mount } from 'enzyme'
-import createRouterContext from 'react-router-test-context'
+import { shallow } from 'enzyme'
 import StartPage from '../../pages/start'
-import RecentMedia from '../../pages/recent'
 import OptionsList from '../../components/OptionsList'
 
 describe('StartPage Component', function() {
-	// const mockStore = configureStore()
-	// let store
-
-	// beforeEach(() => {
-	// 	store = mockStore({});
-	// })
-
-	StartPage.contextTypes = {
-    router: React.PropTypes.object
-  }
-
 	it('should render one <OptionsList> Component', () => {
 		const wrapper = shallow(<StartPage/>)
 		expect(wrapper.find(OptionsList)).to.have.length(1)
-	})
-
-	it('should render one <RecentMedia> Component', () => {
-		const context = createRouterContext()
-
-		const wrapper = shallow(<StartPage/>, { context })
-		expect(wrapper.find(RecentMedia)).to.have.length(1)
 	})
 })
