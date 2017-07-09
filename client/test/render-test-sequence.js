bdf010a022e81458f9c133f9f758ee18c6ec992a
Rolando Ramos <rolandorramos@gmail.com>
2017-07-09 13:55:35 +0200

    Added react-router suggestion for testing component navigation

---
+++ client/test/render-test-sequence.js
@@ -0,0 +1,61 @@
+import { render, unmountComponentAtNode } from 'react-dom'
+import React from 'react'
+import { Route, Link, MemoryRouter } from 'react-router-dom'
+import { Simulate } from 'react-addons-test-utils'
+
+// a way to render any part of your app inside a MemoryRouter
+// you pass it a list of steps to execute when the location
+// changes, it will call back to you with stuff like
+// `match` and `location`, and `history` so you can control
+// the flow and make assertions.
+export const renderTestSequence = ({
+  initialEntries,
+  initialIndex,
+  subject: Subject,
+  steps
+}) => {
+  const div = document.createElement('div')
+
+  class Assert extends React.Component {
+
+    componentDidMount() {
+      this.assert()
+    }
+
+    componentDidUpdate() {
+      this.assert()
+    }
+
+    assert() {
+      const nextStep = steps.shift()
+      if (nextStep) {
+        nextStep({ ...this.props, div })
+      } else {
+        unmountComponentAtNode(div)
+      }
+    }
+
+    render() {
+      return this.props.children
+    }
+  }
+
+  class Test extends React.Component {
+    render() {
+      return (
+        <MemoryRouter
+          initialIndex={initialIndex}
+          initialEntries={initialEntries}
+        >
+          <Route render={(props) => (
+            <Assert {...props}>
+              <Subject/>
+            </Assert>
+          )}/>
+        </MemoryRouter>
+      )
+    }
+  }
+
+  render(<Test/>, div)
+}
