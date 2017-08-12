6ff034f18f21837da190a5c1900660f327e415fe
Rolando Ramos <rolandorramos@gmail.com>
2017-08-12 23:56:32 +0200

    Modified Start page to use RecentMedia HOC

--- client/pages/start/index.js
+++ client/pages/start/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import RecentMediaPage from '../recent';
-import OptionsList from '../../components/OptionsList';
+import React from 'react'
+import { Route } from 'react-router-dom'
+import RecentMedia from '../../containers/RecentMedia'
+import LikesGraph from '../../components/LikesGraph'
+import GraphList from '../../components/GraphList'
 
 class StartPage extends React.Component {
 	render() {
@@ -9,11 +10,10 @@ class StartPage extends React.Component {
 			<div className="startpage">
 				This is the Start Page!
 				<GraphList />
-				<Route path="/start/likes" component={RecentMediaPage} />
-				<Route path="/start/comments" component={RecentMediaPage} />
+				<Route path="/start/likes" component={RecentMedia(LikesGraph)} />
 			</div>
 		)
 	}
 }
 
-export default StartPage;
+export default StartPage
