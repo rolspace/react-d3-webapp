6ae491d13925a157b8e3d6392f72c0ec1269a4f3
Rolando Ramos <rolandorramos@gmail.com>
2017-08-13 11:16:03 +0200

    Added new component to render a graph for the comments count

---
+++ client/components/CommentsGraph.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import * as d3 from 'd3'
+
+class CommentsGraph extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.renderChart = this.renderChart.bind(this);
+	}
+
+	renderChart() {
+		const node = this.node;
+
+		const margins = { top: 20, right: 40, bottom: 20, left: 40 };
+		const width = +d3.select(node).attr('width') - margins.right - margins.left;
+		const height = +d3.select(node).attr('height') - margins.top - margins.bottom;
+		const xDomain = this.props.media.map(media => media.createdTime);
+
+		const x = d3.scaleBand().domain(xDomain).rangeRound([0, width]).padding(0.1);
+		const y = d3.scaleLinear().domain([0, d3.max(this.props.media, d => d.comments.count)]).rangeRound([height, 0]);
+
+		const topG = d3.select(node).append('g')
+			.attr('transform', 'translate(40, 20)');
+
+		topG.append('g').attr('class', 'axis axis--x')
+			.attr('transform', 'translate(0, 460)')
+			.call(d3.axisBottom(x));
+
+		topG.append('g').attr('class', 'axis axis--y')
+			.call(d3.axisLeft(y).ticks(9));
+
+		topG.selectAll('bar').data(this.props.media)
+			.enter().append('rect')
+			.style('fill', 'blue')
+			.attr('x', d => x(d.createdTime))
+			.attr('width', x.bandwidth())
+			.attr('y', d => y(d.comments.count))
+			.attr('height', d => height - y(d.comments.count));
+	}
+
+	componentDidUpdate() {
+		this.renderChart()
+	}
+
+	render() {
+		return (
+			<div>
+				<svg ref={ node => this.node = node} style={{border:'1px solid'}} width={800} height={500}>
+				</svg>
+			</div>
+		)
+	}
+}
+
+CommentsGraph.propTypes = {
+	media: PropTypes.array,
+	user: PropTypes.object
+}
+
+export default CommentsGraph;
--- client/pages/start/index.js
+++ client/pages/start/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import RecentMedia from '../../containers/RecentMedia'
 import LikesGraph from '../../components/LikesGraph'
+import CommentsGraph from '../../components/CommentsGraph'
 import GraphList from '../../components/GraphList'
 
 class StartPage extends React.Component {
@@ -11,6 +12,7 @@ class StartPage extends React.Component {
 				This is the Start Page!
 				<GraphList />
 				<Route path="/start/likes" component={RecentMedia(LikesGraph)} />
+				<Route path="/start/comments" component={RecentMedia(CommentsGraph)} />
 			</div>
 		)
 	}
