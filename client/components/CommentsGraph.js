6f66195fa3eec8ccc3e60d1d3e268c6dd23268ed
Rolando Ramos <rolandorramos@gmail.com>
2017-08-25 00:10:54 +0200

    Added separate components to generate chart for total likes and comments for recent media

--- client/components/CommentsGraph.js
+++ client/components/CommentsGraph.js
@@ -2,43 +2,24 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import * as d3 from 'd3'
+import BarChart from '../common/barchart'
 
 class CommentsGraph extends React.Component {
 	constructor(props) {
-		super(props);
-
-		this.renderChart = this.renderChart.bind(this);
+		super(props)
+		this.renderChart = this.renderChart.bind(this)
 	}
 
 	renderChart() {
 		const node = this.node;
 
-		const margins = { top: 20, right: 40, bottom: 20, left: 40 };
-		const width = +d3.select(node).attr('width') - margins.right - margins.left;
-		const height = +d3.select(node).attr('height') - margins.top - margins.bottom;
-		const xDomain = this.props.media.map(media => media.createdTime);
+		const barchart = {
+			data: this.props.media,
+			xAxis: 'createdTime',
+			yAxis: 'comments.count'
+		}
 
-		const x = d3.scaleBand().domain(xDomain).rangeRound([0, width]).padding(0.1);
-		const y = d3.scaleLinear().domain([0, d3.max(this.props.media, d => d.comments.count)]).rangeRound([height, 0]);
-
-		const topG = d3.select(node).append('g')
-			.attr('transform', 'translate(40, 20)');
-
-		topG.append('g').attr('class', 'axis axis--x')
-			.attr('transform', 'translate(0, 460)')
-			.call(d3.axisBottom(x));
-
-		topG.append('g').attr('class', 'axis axis--y')
-			.call(d3.axisLeft(y).ticks(9));
-
-		topG.selectAll('bar').data(this.props.media)
-			.enter().append('rect')
-			.style('fill', 'blue')
-			.attr('x', d => x(d.createdTime))
-			.attr('width', x.bandwidth())
-			.attr('y', d => y(d.comments.count))
-			.attr('height', d => height - y(d.comments.count));
+		BarChart.create(node, barchart, 800, 500)
 	}
 
 	componentDidUpdate() {
@@ -48,7 +29,7 @@ class CommentsGraph extends React.Component {
 	render() {
 		return (
 			<div>
-				<svg ref={ node => this.node = node} style={{border:'1px solid'}} width={800} height={500}>
+				<svg ref={node => this.node = node} style={{border:'1px solid'}} width={800} height={500}>
 				</svg>
 			</div>
 		)
@@ -60,4 +41,4 @@ CommentsGraph.propTypes = {
 	user: PropTypes.object
 }
 
-export default CommentsGraph;
+export default CommentsGraph
