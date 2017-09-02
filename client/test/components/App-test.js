70a28d1a87b874dec25265088655f621efe19f71
Rolando Ramos <rolandorramos@gmail.com>
2017-09-02 15:32:31 +0200

    Updated App component test to reflect application changes

--- client/test/components/App-test.js
+++ client/test/components/App-test.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import App from '../../components/App'
-import PrivateRoute from '../../containers/PrivateRoute'
+import AppRouter from '../../components/AppRouter'
+import AppBar from 'material-ui/AppBar'
 
-describe('App Component', function() {
-	it('should render one <Provider/> component', () => {
-		const wrapper = shallow(<App/>)
-		expect(wrapper.find(Provider)).to.have.length(1)
+describe('Components: App Component', function() {
+	it('renders one <AppBar> component', () => {
+		const wrapper = shallow(<App />)
+		expect(wrapper.find(AppBar)).to.have.length(1)
 	})
 
-	it('should render one <PrivateRoute/> component', () => {
-		const wrapper = shallow(<App/>)
-		expect(wrapper.find(PrivateRoute)).to.have.length(1)
+	it('renders one <AppRouter> component', () => {
+		const wrapper = shallow(<App />)
+		expect(wrapper.find(AppRouter)).to.have.length(1)
 	})
 })
