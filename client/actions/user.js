fe684144c9cbd96d6aeb197045f1a2028f739762
Rolando Ramos <rolandorramos@gmail.com>
2017-10-22 00:42:19 +0200

    Removed semicolons in user actions

--- client/actions/user.js
+++ client/actions/user.js
@@ -1,40 +1,40 @@
 /* eslint-disable no-unused-vars */
 
-import { createAction } from 'redux-actions';
-import Cookies from 'js-cookie';
-import 'whatwg-fetch';
-import jsonapi from '../common/jsonapi';
+import { createAction } from 'redux-actions'
+import Cookies from 'js-cookie'
+import 'whatwg-fetch'
+import jsonapi from '../common/jsonapi'
 
-export const FETCH_USER = 'FETCH_USER';
-export const FETCH_USER_ERROR = 'FETCH_USER_ERROR';
-export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
+export const FETCH_USER = 'FETCH_USER'
+export const FETCH_USER_ERROR = 'FETCH_USER_ERROR'
+export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS'
 
-export const LOGIN_USER = 'LOGIN_USER';
-export const LOGIN_USER_ERROR = 'LOGIN_USER_ERROR';
-export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
+export const LOGIN_USER = 'LOGIN_USER'
+export const LOGIN_USER_ERROR = 'LOGIN_USER_ERROR'
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
 
 export function getUser() {
 	return (dispatch) => {
-		dispatch(fetchUser());
+		dispatch(fetchUser())
 
-		const id = Cookies.get('id');
+		const id = Cookies.get('id')
 		if (id) {
 			dispatch(fetchUserSuccess({
 				id: id,
 				login: true
-			}));
+			}))
 		}
 		else {
-			dispatch(fetchUserError());
+			dispatch(fetchUserError())
 		}
 	}
 }
 
 export function authenticateUser(code) {
 	return (dispatch) => {
-		dispatch(loginUser);
+		dispatch(loginUser)
 
-		const body = jsonapi.authorizationSerializer.serialize({ code: code });
+		const body = jsonapi.authorizationSerializer.serialize({ code: code })
 		if (code) {
 			fetch('http://localhost:4000/api/auth/', {
 				method: 'POST',
@@ -45,34 +45,34 @@ export function authenticateUser(code) {
 			})
 			.then(response => {
 				if (response.status === 200) {
-					return response.json();
+					return response.json()
 				}
 				else {
-					dispatch(loginUserError());
+					dispatch(loginUserError())
 				}
 			})
 			.then(json => {
-				return jsonapi.deserialize(json);
+				return jsonapi.deserialize(json)
 			})
 			.then(data => {
-				Cookies.set('id', data.id);
+				Cookies.set('id', data.id)
 
 				dispatch(loginUserSuccess({
 					id: data.id,
 					login: true
-				}));
+				}))
 			})
 			.catch(error => {
-				dispatch(loginUserError());
-			});
+				dispatch(loginUserError())
+			})
 		}
 	}
 }
 
-const fetchUser = createAction(FETCH_USER);
-const fetchUserError = createAction(FETCH_USER_ERROR);
-const fetchUserSuccess = createAction(FETCH_USER_SUCCESS);
+const fetchUser = createAction(FETCH_USER)
+const fetchUserError = createAction(FETCH_USER_ERROR)
+const fetchUserSuccess = createAction(FETCH_USER_SUCCESS)
 
-const loginUser = createAction(LOGIN_USER);
-const loginUserError = createAction(LOGIN_USER_ERROR);
-const loginUserSuccess = createAction(LOGIN_USER_SUCCESS);
+const loginUser = createAction(LOGIN_USER)
+const loginUserError = createAction(LOGIN_USER_ERROR)
+const loginUserSuccess = createAction(LOGIN_USER_SUCCESS)
