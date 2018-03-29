b675dd74af8a4905a48e5114cc91eaabddae0c66
Rolando Ramos <rolandorramos@gmail.com>
2018-03-29 21:54:36 +0200

    Added theme to wrap entire App component

--- client/main.js
+++ client/main.js
@@ -2,13 +2,33 @@ import 'babel-polyfill'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
 import store from './common/store'
 import App from './components/App'
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#ff6659',
+			main: '#d32f2f',
+			dark: '#9a0007',
+			contrastText: '#fff',
+		},
+		secondary: {
+			light: '#ff7961',
+			main: '#f44336',
+			dark: '#ba000d',
+			contrastText: '#000',
+		},
+	},
+})
 
 const container = document.getElementById('container')
 
 render(
 	<Provider store={store}>
-    <App />
-  </Provider>, container
+		<MuiThemeProvider theme={theme}>
+			<App />
+		</MuiThemeProvider>
+	</Provider>, container
 )
