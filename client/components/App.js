3909e44ce4ab6e64d2a16fae065dfc2ad7a0cf59
Rolando Ramos <rolandorramos@gmail.com>
2018-03-30 21:39:24 +0200

    Converted App component to functional component and rearranged Material UI grid

--- client/components/App.js
+++ client/components/App.js
@@ -1,30 +1,39 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Typography from 'material-ui/Typography'
 import Toolbar from 'material-ui/Toolbar'
 import AppBar from 'material-ui/AppBar'
 import Grid from 'material-ui/Grid'
 import AppRouter from './AppRouter'
 
-class App extends React.Component {
-	render() {
-		const style = {
-			paddingTop: '80px'
-		}
-
-		return (
-			<div>
-				<AppBar>
-					<Toolbar>
-						React D3 WebApp
-					</Toolbar>
-				</AppBar>
-				<Grid container style={style}>
-					<Grid item xs={12}>
-							<AppRouter />
-					</Grid>
-				</Grid>
-			</div>
-		)
+const styles = {
+	container: {
+		paddingTop: '80px'
 	}
 }
 
-export default App
+const App = (props) => {
+	const { classes } = props
+
+	return (
+		<div>
+			<AppBar>
+				<Toolbar>
+					<Typography variant="title" color="inherit">React D3 WebApp</Typography>
+				</Toolbar>
+			</AppBar>
+			<Grid container className={classes.container} justify='center'>
+				<Grid item xs={12} sm={6}>
+					<AppRouter />
+				</Grid>
+			</Grid>
+		</div>
+	)
+}
+
+App.propTypes = {
+	classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(App)
