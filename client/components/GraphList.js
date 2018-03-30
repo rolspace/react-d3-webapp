077b084d833efb4f9dfe44a58e79f976ac713e4c
Rolando Ramos <rolandorramos@gmail.com>
2018-03-30 21:40:14 +0200

    Converted GraphList to functional component and added Grid and Card components

--- client/components/GraphList.js
+++ client/components/GraphList.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Grid from 'material-ui/Grid'
+import Typography from 'material-ui/Typography'
+import Card, { CardContent } from 'material-ui/Card';
 
-class GraphList extends React.Component {
-	render() {
-		return (
-			<div className="graph-list">
-				<ul>
-					<li><Link to="/graphs/repo-additions-deletions">Repository Addtions/Deletions</Link></li>
-				</ul>
-			</div>
-		)
-	}
+const GraphList = () => {
+	return (
+		<div>
+			<Grid container>
+				<Grid item xs={12} sm={6}>
+					<Link to="/graphs/repo-additions-deletions">
+						<Card>
+							<CardContent>
+								<Typography>Additions vs. Deletions</Typography>
+							</CardContent>
+						</Card>
+					</Link>
+				</Grid>
+			</Grid>
+		</div>
+	)
 }
 
 export default GraphList
