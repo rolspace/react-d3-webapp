c28fb103d7a86f5d2bafe2b71d54974a29f5a8a5
Rolando Ramos <rolandorramos@gmail.com>
2018-04-19 00:06:58 +0200

    Implemented left and right padding to the main container component

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
 import Typography from 'material-ui/Typography'
 import Toolbar from 'material-ui/Toolbar'
 import AppBar from 'material-ui/AppBar'
 import Grid from 'material-ui/Grid'
 import AppRouter from './AppRouter'
 
+const styles = theme => ({
+	container: {
+		[theme.breakpoints.up('md')]: {
+			paddingLeft: '80px',
+			paddingRight: '80px'
+		}
+	}
+})
 
-const App = () => {
+const App = (props) => {
+	const { classes } = props
+
 	return (
 		<div>
 			<AppBar>
@@ -14,7 +26,7 @@ const App = () => {
 					<Typography variant="title" color="inherit">React D3 WebApp</Typography>
 				</Toolbar>
 			</AppBar>
-			<Grid container>
+			<Grid container className={classes.container}>
 				<Grid item xs={12} sm={12}>
 					<AppRouter />
 				</Grid>
@@ -23,4 +35,8 @@ const App = () => {
 	)
 }
 
-export default App
+App.propTypes = {
+	classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(App)
