930fa84500f3dbcf24294733145313b717b6a8b3
Rolando Ramos <rolandorramos@gmail.com>
2018-05-21 21:50:42 +0200

    Removed unnecessary eslint disable rule

--- client/components/BarGraphAddsDeletes.js
+++ client/components/BarGraphAddsDeletes.js
@@ -1,8 +1,5 @@
-/* eslint-disable no-unused-vars */
-
 import React from 'react'
 import PropTypes from 'prop-types'
-import Grid from 'material-ui/Grid'
 import BarGraph from './BarGraph'
 
 class BarGraphAddsDeletes extends React.Component {
