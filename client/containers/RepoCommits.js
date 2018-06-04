712920d6210ef7b5fd58d586e8d720915d39b071
Rolando Ramos <rolandorramos@gmail.com>
2018-06-04 23:05:14 +0200

    Cleaned up RepoCommits container

--- client/containers/RepoCommits.js
+++ client/containers/RepoCommits.js
@@ -3,36 +3,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import _ from 'lodash'
-import { getRepo, updateRepo } from '../actions/repo'
+import { getRepo } from '../actions/repo'
 
 class RepoCommits extends React.Component {
 	constructor(props) {
 		super(props)
 	}
 
-	shouldComponentUpdate(nextProps) {
-		if (!_.isEqual(this.props.repo.data, nextProps.repo.data)) {
-			return true
-		}
-
-		return false
-	}
-
-	componentDidUpdate() {
-		if (!this.props.repo.data.lastId && !this.props.repo.data.lastDate) {
+	componentDidMount() {
+		if (!this.props.repo.data.lastId) {
 			const { dispatch } = this.props
-			const { owner } = this.props.repo.data
-			const { name } = this.props.repo.data
+			const owner = this.props.repo.data.owner || 'facebook'
+			const name = this.props.repo.data.name || 'react'
 			dispatch(getRepo(owner, name))
 		}
-	}
-
-	componentDidMount() {
-		const { dispatch } = this.props
-		const { owner } = this.props.repo.data
-		const { name } = this.props.repo.data
-		dispatch(updateRepo(owner || 'facebook', name || 'react'))
 	}
 
 	render() {
