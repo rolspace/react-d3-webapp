ea8e1e6b5bc307bcc468ea4c023af8304fe4879e
Rolando Ramos <rolandorramos@gmail.com>
2018-08-01 19:09:41 +0200

    Modified test setup based on mocha/chai/sinon to use jest

--- client/test/setup.js
+++ client/test/setup.js
@@ -1,21 +1,4 @@
-require('babel-register')();
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-var jsdom = require('jsdom').jsdom;
-var exposedProperties = ['window', 'navigator', 'document'];
-
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-	if (typeof global[property] === 'undefined') {
-		exposedProperties.push(property);
-		global[property] = document.defaultView[property];
-	}
-});
-
-global.navigator = {
-	userAgent: 'node.js'
-};
-
-global.fetch = function() {}
-
-documentRef = document;
+configure({ adapter: new Adapter() });
