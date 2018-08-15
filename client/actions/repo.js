93f48e24192de1c53d0e6c8b729668e51e1598ca
Rolando Ramos <rolandorramos@gmail.com>
2018-08-16 00:24:21 +0200

    Reformatted file indentation

--- client/actions/repo.js
+++ client/actions/repo.js
@@ -1,53 +1,49 @@
 import 'whatwg-fetch'
-import { createAction } from 'redux-actions'
+import {
+  createAction
+} from 'redux-actions'
 import * as types from './repoTypes'
 
-// export const FETCHING_REPOSITORY = 'FETCHING_REPOSITORY'
-// export const FETCH_REPOSITORY_ERROR = 'FETCH_REPOSITORY_ERROR'
-// export const FETCH_REPOSITORY_SUCCESS = 'FETCH_REPOSITORY_SUCCESS'
-// export const CHANGING_REPOSITORY = 'CHANGING_REPOSITORY'
-// export const CHANGE_REPOSITORY_SUCCESS = 'CHANGE_REPOSITORY_SUCCESS'
-
 export const fetchRepository = (owner, name) => {
-	return (dispatch) => {
-		dispatch(fetchingRepository())
-		return fetch(`${process.env.BACKEND_URL}/api/repository/commits/${owner}/${name}/`)
-    .then(response => {
-      if (response.status === 200) {
-        return response.json()
-      }
-      else {
-        dispatch(fetchRepositoryError())
-      }
-    })
-    .then(json => {
-      return json
-    })
-    .then(result => {
-      const payload = {
-        owner: owner,
-        name: name,
-        data: result.data
-      }
-      dispatch(fetchRepositorySuccess(payload))
-    })
-    .catch(error => {
-      dispatch(fetchRepositoryError(error))
-    })
-	}
+  return (dispatch) => {
+    dispatch(fetchingRepository())
+
+    return fetch(`${process.env.BACKEND_URL}/api/repository/commits/${owner}/${name}/`)
+      .then(response => {
+        if (response.status === 200) {
+          return response.json()
+        } else {
+          dispatch(fetchRepositoryError())
+        }
+      })
+      .then(json => {
+        return json
+      })
+      .then(result => {
+        const payload = {
+          owner: owner,
+          name: name,
+          data: result.data
+        }
+        dispatch(fetchRepositorySuccess(payload))
+      })
+      .catch(error => {
+        dispatch(fetchRepositoryError(error))
+      })
+  }
 }
 
-export const changeRepo = (owner, name) => {
-	return (dispatch) => {
-		dispatch(changingRepo())
-    
-		const payload = {
-			owner: owner,
-			name: name
-		}
-    
-		dispatch(changeRepoSuccess(payload))
-	}
+export const changeRepository = (owner, name) => {
+  return (dispatch) => {
+    dispatch(changingRepo())
+
+    const payload = {
+      owner: owner,
+      name: name
+    }
+
+    dispatch(changeRepoSuccess(payload))
+  }
 }
 
 const fetchingRepository = createAction(types.FETCHING_REPOSITORY)
