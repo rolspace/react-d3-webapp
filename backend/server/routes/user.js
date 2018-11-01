5c7b45863e447256099108a36a4e3f385fe1a3ed
Rolando Ramos <rolandorramos@gmail.com>
2018-11-01 01:30:11 +0100

    Added user route module

---
+++ backend/server/routes/user.js
@@ -0,0 +1,53 @@
+const rp = require('request-promise-native')
+const utils = require('../../common/utils')
+const AppError = require('../../common/error')
+const httpStatus = require('../../common/constants').http
+
+const logger = utils.logger
+
+const postToken = (req, res) => {
+  const { code, state } = req.body
+
+  if (!code || !state ) {
+    logger.error({ message: `token.postToken() error: parameter ${!code ? 'code' : 'state'} is empty`, request: req })
+    const appError = new AppError({
+      message: `The parameter ${!code ? 'code' : 'state'} is empty`,
+      status: httpStatus.unprocessable
+    })
+
+    return res.status(appError.status).send(appError)
+  }
+
+  const options = {
+    method: 'POST',
+    uri: 'https://github.com/login/oauth/access_token',
+    headers: {
+      'Accept': 'application/json'
+    },
+		formData: {
+      client_id: '5757b639c3b7cf82d4ca',
+      client_secret: '5327ec4fd8f2f7e57766cd96696e200abb0a5007',
+      code: code,
+      state: state
+		}
+	}
+
+  rp.post(options)
+  .then(json => {
+    res.status(httpStatus.ok).send(json)
+  })
+  .catch(error => {
+    logger.error({ message: 'repository.getCommits() error: Github request failed', error: error, request: req })
+
+    const appError = new AppError({
+			message: 'Internal server error',
+			status: httpStatus.internalError
+		})
+
+    res.status(appError.status).send(appError)
+  })
+}
+
+module.exports = {
+  postToken
+}
