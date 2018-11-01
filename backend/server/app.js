f8d20a17791fd2086e05f7010b19bc167f1fabd8
Rolando Ramos <rolandorramos@gmail.com>
2018-11-01 01:29:15 +0100

    Slight modification to server startup function

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,10 +1,10 @@
-const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const utils = require('../common/utils')
 const constants = require('../common/constants')
 const repository = require('./routes/repository')
+const user = require('./routes/user')
 
 const queries = require('../common/queries')
 
@@ -18,6 +18,9 @@ const init = () => {
 	app.use(cors(constants.cors))
 	
 	app.get('/api/repository/commits/:owner/:name', repository.getCommits)
+
+	app.options('/api/user/token')
+	app.post('/api/user/token', user.postToken)
 	
 	app.use((req, res) => {
 		res.status(HttpStatus.notFound).send({ 'message': 'Resource not found' })
@@ -27,9 +30,7 @@ const init = () => {
 	
 	app.set('port', port)
 	
-	const server = http.createServer(app)
-	
-	server.listen(port, () => {
+	app.listen(port, () => {
 		logger.info('app.init() info: Server started and listening on port %s', port)
 	})
 }
