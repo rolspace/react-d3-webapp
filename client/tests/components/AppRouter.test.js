3d4cff9bc149ae612ca3548401fc8c781afc285a
Rolando Ramos <rolandorramos@gmail.com>
2019-05-30 14:59:21 +0200

    Updated unit test to reflect correct expectations

--- client/tests/components/AppRouter.test.js
+++ client/tests/components/AppRouter.test.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { Switch, Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+import PrivateRoute from '../../containers/PrivateRoute'
 import AppRouter from '../../components/AppRouter'
 
 describe('Components: AppRouter component', () => {
   it('renders correctly', () => {
     const component = shallow(<AppRouter />)
-    
+
     expect(component.find(Switch).length).toBe(1)
-    expect(component.find(Route).length).toBe(3)
+    expect(component.find(PrivateRoute).length).toBe(2)
   })
 })
