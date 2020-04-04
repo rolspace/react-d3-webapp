1209a367d68dccd631902cdf7e1172fd8fe1c256
Rolando Ramos <rolandorramos@gmail.com>
2020-04-04 23:21:26 +0200

    Setup tests to run in both projects from root folder

---
+++ src/client/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  setupFiles: [
+    "./jest.setup.js"
+  ],
+  testEnvironment: "jsdom",
+  transform: {
+    '\\.js$': ['babel-jest', { cwd: __dirname }],
+  },
+  testURL: "http://localhost"
+}
