04a593b6b100e610420a8f6a6e989496ca9b89e7
Roland <rolandorramos@gmail.com>
2022-03-24 23:11:57 +0100

    Fixed linting issues

--- src/client/webpack.dev.js
+++ src/client/webpack.dev.js
@@ -1,30 +1,30 @@
 /* eslint-disable global-require */
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 
 const config = {
-    mode: 'development',
-    devtool: 'source-map',
-    stats: {
-        reasons: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css/,
-                use: [
-                    'style-loader',
-                    `css-loader?${JSON.stringify({
-                        sourceMap: true,
-                        modules: true,
-                        localIdentName: '[name]_[local]_[hash:base64:3]',
-                        minimize: false,
-                    })}`,
-                    'postcss-loader',
-                ],
-            },
-        ]
-    }
+  mode: 'development',
+  devtool: 'source-map',
+  stats: {
+    reasons: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          `css-loader?${JSON.stringify({
+            sourceMap: true,
+            modules: true,
+            localIdentName: '[name]_[local]_[hash:base64:3]',
+            minimize: false,
+          })}`,
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
 }
 
 module.exports = merge(common, config)
--- src/client/webpack.prod.js
+++ src/client/webpack.prod.js
@@ -1,27 +1,27 @@
 /* eslint-disable global-require */
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 
 const config = {
-    mode: 'production',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.css/,
-                use: [
-                    'style-loader',
-                    `css-loader?${JSON.stringify({
-                        sourceMap: false,
-                        modules: true,
-                        localIdentName: '[name]_[local]_[hash:base64:3]',
-                        minimize: true,
-                    })}`,
-                    'postcss-loader',
-                ],
-            },
-        ]
-    }
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          `css-loader?${JSON.stringify({
+            sourceMap: false,
+            modules: true,
+            localIdentName: '[name]_[local]_[hash:base64:3]',
+            minimize: true,
+          })}`,
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
 }
 
 module.exports = merge(common, config)
