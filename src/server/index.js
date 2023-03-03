89acd000230d7787656899f654d75de35168128e
Roland <rolandorramos@gmail.com>
2023-03-03 14:34:17 +0100

    Do not use DotEnv on production

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,7 @@
 // TODO: convert api server completely to import based
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const bodyParser = require('body-parser')
 const cors = require('cors')
