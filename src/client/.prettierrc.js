6eee3c03d2bc30d8c5ab2df74acc0cb25dd73ef7
Roland <rolandorramos@gmail.com>
2023-03-20 00:03:16 +0100

    Changes prettier config to use two space tab width

--- src/client/.prettierrc.js
+++ src/client/.prettierrc.js
@@ -2,6 +2,7 @@ const prettierConfigStandard = require('prettier-config-standard')
 
 const prettierConfig = Object.assign({}, prettierConfigStandard, {
   bracketSameLine: true,
+  tabWidth: 2,
   trailingComma: 'all',
 })
 
