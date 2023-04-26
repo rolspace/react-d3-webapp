f2a68059eda9eb07989ae98e08da5ea7b5515d6f
Roland <rolandorramos@gmail.com>
2023-04-26 23:58:01 +0200

    Added test for App component

---
+++ src/client/components/__tests__/App.test.js
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import App from '../App'
+import userEvent from '@testing-library/user-event'
+
+test('App renders correctly', async () => {
+  render(<App />)
+
+  expect(
+    screen.getByRole('heading', {
+      name: /gh repositories \/ charts and data/i,
+    }),
+  ).toBeInTheDocument()
+  expect(screen.getAllByRole('listitem').length).toEqual(4)
+  expect(screen.getAllByRole('link').length).toEqual(5)
+  expect(
+    screen.queryByRole('presentation', {
+      hidden: true,
+    }),
+  ).toBeNull()
+
+  const menuButton = screen.getByRole('button', {
+    name: /menu/i,
+  })
+
+  await userEvent.click(menuButton)
+
+  expect(
+    screen.getByRole('presentation', {
+      hidden: false,
+    }),
+  ).toBeInTheDocument()
+})
