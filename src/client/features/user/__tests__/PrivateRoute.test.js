b28aa4f373c7b78ab92e7fb1aecf6c511d2658dd
Roland <rolandorramos@gmail.com>
2023-05-02 00:38:35 +0200

    Cleaned up test

--- src/client/features/user/__tests__/PrivateRoute.test.js
+++ src/client/features/user/__tests__/PrivateRoute.test.js
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
 import { screen } from '@testing-library/react'
-import React, { Fragment } from 'react'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { renderWithProviders } from '../../../utils/testUtils'
 import PrivateRoute from '../PrivateRoute'
-import { BrowserRouter } from 'react-router-dom'
 
 const location = {
   ...window.location,
@@ -18,7 +18,7 @@ Object.defineProperty(window, 'location', {
 test('PrivateRoute fetches user token and renders correct component', () => {
   renderWithProviders(
     <BrowserRouter>
-      <PrivateRoute component={<></>} path="/path" />
+      <PrivateRoute component={() => <></>} path="/path" />
     </BrowserRouter>,
     {
       preloadedState: {
