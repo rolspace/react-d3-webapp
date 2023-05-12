35ba1fe2d5a931d01618f5e884b93a9a77f0802d
Roland <rolandorramos@gmail.com>
2023-05-12 23:50:51 +0200

    Implemented first draft of tests for repo route

--- src/server/routes/__tests__/repo.test.js
+++ src/server/routes/__tests__/repo.test.js
@@ -1,7 +1,6 @@
-import rp from 'request-promise-native'
-import { logger } from '../../common/logger'
-import { getQuery, loadQueries } from '../../common/queries'
-import { repo } from '../repo'
+import { jest } from '@jest/globals'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 
 let res = {}
 
@@ -19,63 +18,82 @@ afterEach(() => {
 })
 
 test('repo module responds with a 200 status code when the request for data is successful', async () => {
+  const handlers = [
+    rest.post('https://api.github.com/graphql', (req, res, context) => {
+      return res(
+        context.json({
+          data: {
+            repository: { ref: { target: { history: { edges: [] } } } },
+          },
+        }),
+      )
+    }),
+  ]
+
+  const server = setupServer(...handlers)
+  server.listen()
+
   const req = {
     params: { name: 'name', owner: 'owner' },
     body: { token: 'token' },
   }
-  const data = {
-    data: { repository: { ref: { target: { history: { edges: [] } } } } },
-  }
 
-  const queriesMock = jest
-    .spyOn(queries, 'getQuery')
-    .mockReturnValue({ data: 'repository(name: "%NAME%", owner: "%OWNER%")' })
+  jest.unstable_mockModule('../../common/logger', () => ({
+    logger: {
+      info: jest.fn(),
+    },
+  }))
 
-  const requestPromiseMock = jest.spyOn(rp, 'post').mockResolvedValue(data)
+  await import('../../common/logger')
 
-  jest.spyOn(logger, 'info').mockImplementation(() => jest.fn())
+  jest.unstable_mockModule('../../common/queries', () => ({
+    getQuery: jest.fn().mockReturnValue({
+      text: 'repository(name: "%NAME%", owner: "%OWNER%")',
+    }),
+  }))
+  await import('../../common/queries')
 
-  await repo.post(req, res)
+  const { post } = await import('../repo')
 
-  expect(queriesMock).toHaveBeenCalledTimes(1)
-  expect(requestPromiseMock).toHaveBeenCalledTimes(1)
+  await post(req, res, () => {})
+
   expect(res.send).toHaveBeenCalledTimes(1)
   expect(res.status).toHaveBeenCalledTimes(1)
   expect(res.status).toHaveBeenCalledWith(200)
 })
 
-test('repo module calls the next handler if the name parameter is not in the request body', async () => {
-  const req = { params: { owner: 'owner' }, body: { token: 'token' } }
+// test('repo module calls the next handler if the name parameter is not in the request body', async () => {
+//   const req = { params: { owner: 'owner' }, body: { token: 'token' } }
 
-  const queriesMock = jest
-    .spyOn(queries, 'getQuery')
-    .mockReturnValue({ data: 'repository(name: "%NAME%", owner: "%OWNER%")' })
+//   const queriesMock = jest
+//     .spyOn(queries, 'getQuery')
+//     .mockReturnValue({ data: 'repository(name: "%NAME%", owner: "%OWNER%")' })
 
-  const nextMock = jest.fn()
+//   const nextMock = jest.fn()
 
-  await repo.post(req, res, nextMock)
+//   await repo.post(req, res, nextMock)
 
-  expect(queriesMock).toHaveBeenCalledTimes(1)
-  expect(nextMock).toHaveBeenCalledTimes(1)
-})
+//   expect(queriesMock).toHaveBeenCalledTimes(1)
+//   expect(nextMock).toHaveBeenCalledTimes(1)
+// })
 
-test('repo module calls the next handler, if there an error retrieving the external data', async () => {
-  const req = {
-    params: { name: 'name', owner: 'owner' },
-    body: { token: 'token' },
-  }
+// test('repo module calls the next handler, if there an error retrieving the external data', async () => {
+//   const req = {
+//     params: { name: 'name', owner: 'owner' },
+//     body: { token: 'token' },
+//   }
 
-  const queriesMock = jest
-    .spyOn(queries, 'getQuery')
-    .mockReturnValue({ data: 'repository(name: "%NAME%", owner: "%OWNER%")' })
+//   const queriesMock = jest
+//     .spyOn(queries, 'getQuery')
+//     .mockReturnValue({ data: 'repository(name: "%NAME%", owner: "%OWNER%")' })
 
-  const requestPromiseMock = jest.spyOn(rp, 'post').mockRejectedValue('error')
+//   const requestPromiseMock = jest.spyOn(rp, 'post').mockRejectedValue('error')
 
-  const nextMock = jest.fn()
+//   const nextMock = jest.fn()
 
-  await repo.post(req, res, nextMock)
+//   await repo.post(req, res, nextMock)
 
-  expect(queriesMock).toHaveBeenCalledTimes(1)
-  expect(requestPromiseMock).toHaveBeenCalledTimes(1)
-  expect(nextMock).toHaveBeenCalledTimes(1)
-})
+//   expect(queriesMock).toHaveBeenCalledTimes(1)
+//   expect(requestPromiseMock).toHaveBeenCalledTimes(1)
+//   expect(nextMock).toHaveBeenCalledTimes(1)
+// })
