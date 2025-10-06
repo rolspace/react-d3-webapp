567f478e39bccd05a71c7ebe9aa421decac47045
Roland <10880743+rolspace@users.noreply.github.com>
2025-10-06 11:32:25 +0200

    feat: Converted AppRouter to TypeScript

--- src/client/components/AppRouter.tsx
+++ src/client/components/AppRouter.tsx
@@ -1,28 +1,22 @@
 import React from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import RepoCommits from '../features/repo/RepoCommits.jsx'
-import PrivateRoute from '../features/user/PrivateRoute.jsx'
-import GraphPage from '../pages/GraphPage.jsx'
-import HomePage from '../pages/HomePage.jsx'
+import RepoCommits from '../features/repo/RepoCommits'
+import PrivateRoute from '../features/user/PrivateRoute'
+import GraphPage from '../pages/GraphPage'
+import HomePage from '../pages/HomePage'
 import BarGraphAddsDeletes from './BarGraphAddsDeletes'
 import BarGraphChangedFiles from './BarGraphChangedFiles'
 
-const options = {
-  xAxis: 'label',
-  yAxis: 'count',
-}
-
-const AppRouter = () => {
+const AppRouter: React.FC = () => {
   const { pathname, search } = useLocation()
 
-  const AddsDeletes = GraphPage(RepoCommits, BarGraphAddsDeletes, options)
-  const ChangedFiles = GraphPage(RepoCommits, BarGraphChangedFiles, options)
+  const AddsDeletes = GraphPage(RepoCommits, BarGraphAddsDeletes)
+  const ChangedFiles = GraphPage(RepoCommits, BarGraphChangedFiles)
 
   return (
     <Routes>
-      <Route exact path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route
-        exact
         path="/graphs/repo-additions-deletions"
         element={
           <PrivateRoute
@@ -35,7 +29,6 @@ const AppRouter = () => {
         }
       />
       <Route
-        exact
         path="/graphs/repo-files"
         element={
           <PrivateRoute
