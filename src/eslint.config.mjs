f80af0427ecba79aba4c8cfeca431738b0b6cfc5
Roland <10880743+rolspace@users.noreply.github.com>
2025-12-21 23:38:48 +0100

    fix: Added missing config to new setup for eslint

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,6 +1,69 @@
 import neostandard from 'neostandard'
 
-export default neostandard({
-  ignores: ['dist', 'coverage', 'public/dist', 'node_modules'],
-  ts: true,
-})
+export default [
+  ...neostandard({
+    env: ['node', 'browser', 'jest'],
+    ignores: ['dist', 'coverage', 'public/dist', 'node_modules'],
+    ts: true,
+  }),
+  plugins.react.configs.flat.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react: plugins.react,
+    },
+    rules: {
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/jsx-closing-bracket-location': [
+        1,
+        {
+          nonEmpty: 'tag-aligned',
+          selfClosing: 'line-aligned',
+        },
+      ],
+      '@stylistic/jsx-quotes': ['error', 'prefer-double'],
+      '@stylistic/multiline-ternary': 'off',
+      '@stylistic/space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'always',
+          named: 'never',
+          asyncArrow: 'always',
+        },
+      ],
+      'brace-style': [
+        'error',
+        '1tbs',
+        {
+          allowSingleLine: false,
+        },
+      ],
+      curly: ['error', 'all'],
+      'prefer-destructuring': [
+        'error',
+        {
+          array: true,
+          object: true,
+        },
+        {
+          enforceForRenamedProperties: true,
+        },
+      ],
+      quotes: ['error', 'single'],
+      'react/prop-types': [
+        'error',
+        {
+          ignore: ['location', 'query', 'code'],
+        },
+      ],
+    },
+  },
+]
