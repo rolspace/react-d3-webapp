f150aded2825d4cfbd10676176d07d430fbb50f6
Roland <10880743+rolspace@users.noreply.github.com>
2025-12-22 23:07:10 +0100

    fix: Fixed an issue with prod frontend build

--- src/webpack/webpack.prod.ts
+++ src/webpack/webpack.prod.ts
@@ -1,12 +1,12 @@
 import { merge } from 'webpack-merge'
-import { Configuration, DefinePlugin } from 'webpack'
+import webpack, { Configuration } from 'webpack'
 import common from './webpack.common.js'
 
 const config: Configuration = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    new DefinePlugin({
+    new webpack.DefinePlugin({
       'process.env.API_URL': JSON.stringify(
         process.env.API_URL || 'https://localhost:3000',
       ),
