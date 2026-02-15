3b7001a508f89e77fb79eaaaa39999061cd00311
Roland <10880743+rolspace@users.noreply.github.com>
2026-02-15 17:20:03 +0100

    fix: Cleanup vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     projects: [
-      'src/client/vitest.config.mjs',
-      'src/api/vitest.config.mjs',
-      'src/vitest.config.ts',
+      'src/vitest.config.backend.ts',
+      'src/vitest.config.frontend.ts',
     ],
   },
 })
